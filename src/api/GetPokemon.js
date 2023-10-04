fba779fceb1e736ef5b1c9c2ffd5a782f428fc92
kadeem-lewis <email@kadeemlewis.com>
2023-10-04 17:37:20 -0400

    added function to fetch Pokedex and pokemon from pokeapi

---
+++ src/api/GetPokemon.js
@@ -0,0 +1,32 @@
+export async function getPokedex() {
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getPokemon(pokemon) {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    );
+    const data = await response.json();
+    const additionalResponse = await fetch(data.species.url);
+    const additionalData = await additionalResponse.json();
+
+    const flavorText = additionalData.flavor_text_entries.find(
+      (text) => text.language.name === "en"
+    );
+
+    return { ...data, flavorText };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
