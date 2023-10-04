45de39f6d9e4668fa1aa6eb48e5fc6025dcdb706
kadeem-lewis <email@kadeemlewis.com>
2023-10-04 17:39:02 -0400

    card to show all the pokemon data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,43 @@
+import { useContext } from "react";
+import { PokemonContext } from "./PokemonProvider";
+
 export const Card = () => {
-  return <div>Card</div>;
+  const { selectedPokemon } = useContext(PokemonContext);
+  return selectedPokemon ? (
+    <div className="card">
+      <div className="card-heading">Info</div>
+      <div className="card-img">
+        <img
+          src={selectedPokemon.sprites.front_default}
+          alt={`${selectedPokemon.name} sprite`}
+        />
+      </div>
+      <div className="card-data">
+        <div className="card-name">
+          {selectedPokemon.id.toString().padStart(3, "0")}{" "}
+          {selectedPokemon.name}
+        </div>
+        <div className="type-box">
+          {selectedPokemon.types.map((type, index) => (
+            <div key={index} className={`type ${type.type.name}`}>
+              {type.type.name}
+            </div>
+          ))}
+        </div>
+        <div className="physical-stats">
+          <span>HT</span>
+          <span>{selectedPokemon.height / 10}"</span>
+        </div>
+        <div className="physical-stats">
+          <span>WT</span>
+          <span>{(selectedPokemon.weight / 10).toFixed(1)} lbs.</span>
+        </div>
+      </div>
+      <p className="card-description">
+        {selectedPokemon.flavorText.flavor_text}
+      </p>
+    </div>
+  ) : (
+    <p>Search for a pokemon</p>
+  );
 };
