c21509573475e09522739b9254c039e9639424f1
kadeem-lewis <email@kadeemlewis.com>
2023-10-04 17:39:43 -0400

    search function shows all pokemon in datalist and the selected pokemon is used to fetch that pokemon from the api

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,35 @@
+import { useContext } from "react";
+import { PokemonContext } from "./PokemonProvider";
+import { getPokemon } from "../api/GetPokemon";
+
 export const Search = () => {
+  const { pokemon, setSelectedPokemon } = useContext(PokemonContext);
+
+  const handleChange = async (event) => {
+    if (event.target) {
+      const isValid = pokemon.map((p) => p.name).includes(event.target.value);
+      if (isValid) {
+        const selected = await getPokemon(event.target.value);
+        setSelectedPokemon(selected);
+      }
+    }
+  };
   return (
     <>
-      <input type="text" />
+      <label htmlFor="pokemon-search">Search for a pokemon:</label>
+      <input
+        type="text"
+        list="pokedex"
+        id="pokemon-search"
+        onChange={handleChange}
+      />
+      <datalist id="pokedex">
+        {pokemon.map((p) => (
+          <option key={p.name} value={p.name}>
+            {p.name}
+          </option>
+        ))}
+      </datalist>
     </>
   );
 };
