7952f8ce3f1989b5d139f95303351474d19fd51f
kadeem-lewis <email@kadeemlewis.com>
2023-10-31 23:46:01 -0400

    created schema for mongoose model

---
+++ backend/src/models/Pokedex.js
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const PokedexSchema = new Schema({
+  id: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  generation: { type: Number, required: true },
+  types: { type: Array, required: true },
+  sprite: { type: String, required: true },
+  flavorText: { type: String, required: true },
+  height: { type: Number, required: true },
+  weight: { type: Number, required: true },
+});
+
+const Pokedex = model("ksl29_pokedex", PokedexSchema);
+export default Pokedex;
