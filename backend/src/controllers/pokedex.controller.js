9296078e73e17e5821cfb80faaa4b305ea647a49
kadeem-lewis <email@kadeemlewis.com>
2023-11-03 12:08:51 -0400

    fixed wrong name in else check

--- backend/src/controllers/pokedex.controller.js
+++ backend/src/controllers/pokedex.controller.js
@@ -1,13 +1,12 @@
 import Pokedex from "../models/Pokedex.js";
 
 export async function getPokedex(req, res, next) {
-  console.log("Query:", req.query);
   const pokemonPerPage = parseInt(req.query.pokemonPerPage) || 20;
   const page = parseInt(req.query.page) || 0;
   const filters = {};
   if (req.query.id) {
     filters.id = req.query.id;
-  } else if (req.query.title) {
+  } else if (req.query.name) {
     filters.name = req.query.name;
   }
   const results = await Pokedex.find(filters)
