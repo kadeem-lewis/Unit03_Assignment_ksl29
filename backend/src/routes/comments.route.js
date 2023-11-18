9d340fcb875e188685f9237c9deb45817dc65a71
kadeem-lewis <email@kadeemlewis.com>
2023-11-17 21:54:11 -0500

    created a router to handle server routes

---
+++ backend/src/routes/comments.route.js
@@ -0,0 +1,22 @@
+//Kadeem Lewis
+//ksl29
+//11-17-2023
+//IT301 - 001
+//Unit 09 Assignment
+
+import express from "express";
+import {
+  addComment,
+  updateComment,
+  deleteComment,
+} from "../controllers/comment.controller.js";
+
+const router = express.Router();
+
+router
+  .route("/comments")
+  .post(addComment)
+  .put(updateComment)
+  .delete(deleteComment);
+
+export { router as commentRouter };
