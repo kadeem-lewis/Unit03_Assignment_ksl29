02bda4b2a422cd174c847cc06fd1d5dd6efecece
kadeem-lewis <email@kadeemlewis.com>
2023-11-23 04:02:50 -0500

    created a pokemon route

---
+++ frontend/src/routes/pokemon.jsx
@@ -0,0 +1,16 @@
+//Kadeem Lewis
+//ksl29
+//12-01-2023
+//IT301 - 001
+//Unit 03 Assignment
+
+import React from 'react'
+import {Card} from '../components/Card.jsx'
+
+export default function Pokemon() {
+  return (
+    <>
+    <Card />
+    </>
+  )
+}
