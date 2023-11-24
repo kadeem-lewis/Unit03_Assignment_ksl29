92484ea660fe28a5771cdef19e666519a7a55307
kadeem-lewis <email@kadeemlewis.com>
2023-11-24 05:19:43 -0500

    commented most of search code to handle errors

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -8,34 +8,33 @@ import { useOutletContext } from "react-router-dom";
 import { getPokemon } from "../api/GetPokemon";
 
 export const Search = () => {
-  const { pokemon, setSelectedPokemon } = useOutletContext();
 
-  const handleChange = async (event) => {
-    if (event.target) {
-      const isValid = pokemon.map((p) => p.name).includes(event.target.value);
-      if (isValid) {
-        const selected = await getPokemon(event.target.value);
-        setSelectedPokemon(selected);
-      }
-    }
-  };
+  // const handleChange = async (event) => {
+  //   if (event.target) {
+  //     const isValid = pokemon.map((p) => p.name).includes(event.target.value);
+  //     if (isValid) {
+  //       const selected = await getPokemon(event.target.value);
+  //       setSelectedPokemon(selected);
+  //     }
+  //   }
+  // };
   return (
     <>
       <label htmlFor="pokemon-search" className="search">
         Search for a pokemon:
       </label>
-      <input
+      {/* <input
         type="text"
         list="pokedex"
         id="pokemon-search"
         onChange={handleChange}
-      />
+      /> */}
       <datalist id="pokedex">
-        {pokemon.map((p) => (
+        {/* {pokemon.map((p) => (
           <option key={p.name} value={p.name}>
             {p.name}
           </option>
-        ))}
+        ))} */}
       </datalist>
     </>
   );
