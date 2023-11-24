359137148f636e356666a71ba06b9772d9d8f639
kadeem-lewis <email@kadeemlewis.com>
2023-11-24 05:19:06 -0500

    add comments contains the current selected comment to prefill the textarea now

--- frontend/src/routes/addComment.jsx
+++ frontend/src/routes/addComment.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Form, useLocation } from 'react-router-dom'
 
 export default function AddComment() {
+  const {state} = useLocation();
+  let editing = false;
+  let initialCommentState = "";
+
+  if(state && state.currentComment){
+    editing = true;
+    initialCommentState = state.currentComment.comment;
+  }
+
+  const [comment, setComment] = useState(initialCommentState);
+
   return (
-    <div>Add Comment</div>
+    <>
+      <Form method={editing?"PUT":"POST"}>
+        <textarea name="" id="" cols="30" rows="10" value={comment}/>
+        <button type="submit">Submit</button>
+      </Form>
+    </>
   )
+}
+
+
+export async function action({request}){
+
 }
--- frontend/src/routes/comment.jsx
+++ frontend/src/routes/comment.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, Link } from 'react-router-dom'
 import { getComments } from '../api/GetPokemon'
 
 export async function commentsLoader({params}){
@@ -9,10 +9,17 @@ export async function commentsLoader({params}){
 
 export default function Comment() {
   const comments = useLoaderData();
+  const handleDeleteCommentClick = async () => {
+
+  }
   return (
     <>
     {comments?.map((comment) => (
+      <>
       <p key={comment._id}>{comment.comment}</p>
+      <Link to="add-comment" state={{currentComment:comment}}>Edit Comment</Link>
+      <button onClick={()=>handleDeleteCommentClick()}>Delete Comment</button>
+      </>
     ))}
     </>
   )
