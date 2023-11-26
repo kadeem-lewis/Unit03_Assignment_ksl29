3e20d6d6230045dc5b9d41c7fc27b6b5f1aa2da7
kadeem-lewis <email@kadeemlewis.com>
2023-11-26 04:11:08 -0500

    added key to comments to fix error

--- frontend/src/routes/comment.jsx
+++ frontend/src/routes/comment.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { useLoaderData, Link } from 'react-router-dom'
-import { getComments } from '../api/GetPokemon'
+import React, { Fragment } from "react";
+import { useLoaderData, Link } from "react-router-dom";
+import { getComments } from "../api/GetPokemon";
 
-export async function commentsLoader({params}){
-  const comments = await getComments(params.id)
-  return comments
+export async function commentsLoader({ params }) {
+  const comments = await getComments(params.id);
+  return comments;
 }
 
 export default function Comment() {
   const comments = useLoaderData();
-  const handleDeleteCommentClick = async () => {
-
-  }
+  const handleDeleteCommentClick = async () => {};
   return (
     <>
-    {comments?.map((comment) => (
-      <>
-      <p key={comment._id}>{comment.comment}</p>
-      <Link to="add-comment" state={{currentComment:comment}}>Edit Comment</Link>
-      <button onClick={()=>handleDeleteCommentClick()}>Delete Comment</button>
-      </>
-    ))}
+      {comments?.map((comment) => (
+        <Fragment key={comment._id}>
+          <p>{comment.comment}</p>
+          <Link to="add-comment" state={{ currentComment: comment }}>
+            Edit Comment
+          </Link>
+          <button onClick={() => handleDeleteCommentClick()}>
+            Delete Comment
+          </button>
+        </Fragment>
+      ))}
     </>
-  )
+  );
 }
