f1b692645b2cd0e8bb524b8ae9e4328003642407
kadeem-lewis <email@kadeemlewis.com>
2023-11-28 05:08:55 -0500

    passed user prop down to deeper context

--- frontend/src/routes/pokemon.jsx
+++ frontend/src/routes/pokemon.jsx
@@ -4,23 +4,24 @@
 //IT301 - 001
 //Unit 11 Assignment
 
-import React from 'react'
-import {Card} from '../components/Card.jsx'
-import { getPokemon } from '../api/GetPokemon.js'
-import { Outlet, useLoaderData } from 'react-router-dom'
+import React from "react";
+import { Card } from "../components/Card.jsx";
+import { getPokemon } from "../api/GetPokemon.js";
+import { Outlet, useLoaderData, useOutletContext } from "react-router-dom";
 
-export async function pokemonLoader({params}){
-  const pokemon = await getPokemon(params.id)
-  return pokemon
+export async function pokemonLoader({ params }) {
+  const pokemon = await getPokemon(params.id);
+  return pokemon;
 }
 
 export default function Pokemon() {
   const pokemon = useLoaderData();
+  const { user } = useOutletContext();
 
   return (
     <>
-    <Card pokemon={pokemon}/>
-    <Outlet/>
+      <Card pokemon={pokemon} />
+      <Outlet context={user} />
     </>
-  )
+  );
 }
