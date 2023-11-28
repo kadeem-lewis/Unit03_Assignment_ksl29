2d55ea8f6d501e458a31a5e634c5bf07edab9496
kadeem-lewis <email@kadeemlewis.com>
2023-11-28 05:07:05 -0500

    created a duplicate route to handle having pokemon route requirement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,29 +6,36 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Root, { rootLoader } from "./routes/root.jsx";
-import Pokemon, {pokemonLoader} from "./routes/pokemon.jsx";
+import Pokemon, { pokemonLoader } from "./routes/pokemon.jsx";
 import "./index.css";
 import RootLayout from "./layouts/RootLayout.jsx";
-import Comment, {commentsLoader} from "./routes/comment.jsx";
+import Comment, { commentsLoader } from "./routes/comment.jsx";
 import AddComment from "./routes/addComment.jsx";
 import Login from "./routes/login.jsx";
 
-const router = createBrowserRouter(createRoutesFromElements(
-<Route path="/" element={<RootLayout/>}>
-  <Route index element={<Root/>} loader={rootLoader}/>
-  <Route path="ksl29_login" element={<Login/>} />
-  <Route path=":id" element={<Pokemon/>} loader={pokemonLoader}>
-    <Route index element={<Comment/>} loader={commentsLoader}/>
-    <Route path="add-comment" element={<AddComment/>} />
-  </Route>
-
-</Route>
-))
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Root />} loader={rootLoader} />
+      <Route path="ksl29_pokemon" element={<Root />} loader={rootLoader} />
+      <Route path="ksl29_login" element={<Login />} />
+      <Route path=":id" element={<Pokemon />} loader={pokemonLoader}>
+        <Route index element={<Comment />} loader={commentsLoader} />
+        <Route path="add-comment" element={<AddComment />} />
+      </Route>
+    </Route>,
+  ),
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
