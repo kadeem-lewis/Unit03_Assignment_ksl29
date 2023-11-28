a9f7e6608d7e89a1f40a8cf207012a1ecce929aa
kadeem-lewis <email@kadeemlewis.com>
2023-11-28 05:08:09 -0500

    defined pokemon types and created function to update route to fetch them

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -11,13 +11,34 @@ import PokemonCard from "../components/PokemonCard";
 export async function rootLoader({ request }) {
   const url = new URL(request.url);
   const searchParams = url.searchParams.toString();
-  console.log(searchParams);
+  console.log("Search Params:", searchParams);
   const response = await fetch(
     `http://localhost:5000/api/v1/ksl29/pokemon?${searchParams}`,
   );
   const pokedex = await response.json();
   return pokedex.results;
 }
+
+const pokemonTypes = [
+  "normal",
+  "fire",
+  "water",
+  "electric",
+  "grass",
+  "ice",
+  "fighting",
+  "poison",
+  "ground",
+  "flying",
+  "psychic",
+  "bug",
+  "rock",
+  "ghost",
+  "dragon",
+  "dark",
+  "steel",
+  "fairy",
+];
 
 export default function Root() {
   const pokedex = useLoaderData();
@@ -27,6 +48,11 @@ export default function Root() {
     const name = event.target.elements["pokemon-search"].value;
     setSearchParams({ name: name });
   };
+  const handleSelectSubmit = (event) => {
+    event.preventDefault();
+    const type = event.target.elements["pokemon-type"].value;
+    setSearchParams({ type: type });
+  };
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -34,6 +60,19 @@ export default function Root() {
           Search for a pokemon:
         </label>
         <input type="text" list="pokedex" id="pokemon-search" />
+        <button type="submit">Submit</button>
+      </form>
+      <form onSubmit={(e) => handleSelectSubmit(e)}>
+        <label htmlFor="pokemon-type" className="search">
+          Filter by type:
+        </label>
+        <select id="pokemon-type">
+          {pokemonTypes.map((type, index) => (
+            <option key={index} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
         <button type="submit">Submit</button>
       </form>
       <div className="pokedex-grid">
