cd79b325879fc37ea1938da66dc2e451ca4de045
kadeem-lewis <email@kadeemlewis.com>
2023-11-28 05:16:16 -0500

    updated comment structure and added link to add comment

--- frontend/src/routes/comment.jsx
+++ frontend/src/routes/comment.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
-import { useLoaderData, Link } from "react-router-dom";
+import { useLoaderData, Link, useOutletContext } from "react-router-dom";
 import { getComments } from "../api/GetPokemon";
+import styles from "./comment.module.css";
 
 export async function commentsLoader({ params }) {
   const comments = await getComments(params.id);
@@ -9,19 +10,32 @@ export async function commentsLoader({ params }) {
 
 export default function Comment() {
   const comments = useLoaderData();
+  const { user } = useOutletContext();
   const handleDeleteCommentClick = async () => {};
   return (
     <>
+      <Link to="add-comment">Add Comment</Link>
       {comments?.map((comment) => (
-        <Fragment key={comment._id}>
-          <p>{comment.comment}</p>
-          <Link to="add-comment" state={{ currentComment: comment }}>
-            Edit Comment
-          </Link>
-          <button onClick={() => handleDeleteCommentClick()}>
-            Delete Comment
-          </button>
-        </Fragment>
+        <div key={comment._id}>
+          <div className={styles.commentCard}>
+            <span>{comment.username.slice(0, 1).toUpperCase()}</span>
+            <div>
+              <p>{comment.username}</p>
+              <p>{comment.lastModified}</p>
+            </div>
+            <p>{comment.comment}</p>
+          </div>
+          {user?.username === comment?.username && (
+            <>
+              <Link to="add-comment" state={{ currentComment: comment }}>
+                Edit Comment
+              </Link>
+              <button onClick={() => handleDeleteCommentClick()}>
+                Delete Comment
+              </button>
+            </>
+          )}
+        </div>
       ))}
     </>
   );
