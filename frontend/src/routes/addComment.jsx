818f3ca539f518d8deaf58f0a1cf2f735dad010a
kadeem-lewis <email@kadeemlewis.com>
2023-11-28 05:17:50 -0500

    added onChange function for textarea

--- frontend/src/routes/addComment.jsx
+++ frontend/src/routes/addComment.jsx
@@ -1,29 +1,37 @@
-import React, {useState} from 'react'
-import { Form, useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { Form, useLocation } from "react-router-dom";
 
 export default function AddComment() {
-  const {state} = useLocation();
+  const { state } = useLocation();
   let editing = false;
   let initialCommentState = "";
 
-  if(state && state.currentComment){
+  if (state && state.currentComment) {
     editing = true;
     initialCommentState = state.currentComment.comment;
   }
 
   const [comment, setComment] = useState(initialCommentState);
 
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
   return (
     <>
-      <Form method={editing?"PUT":"POST"}>
-        <textarea name="" id="" cols="30" rows="10" value={comment}/>
+      <Form method={editing ? "PUT" : "POST"}>
+        <textarea
+          name="comment"
+          id="comment"
+          cols="30"
+          rows="10"
+          onChange={handleCommentChange}
+          value={comment}
+        />
         <button type="submit">Submit</button>
       </Form>
     </>
-  )
+  );
 }
 
-
-export async function action({request}){
-
-}
+export async function action({ request }) {}
