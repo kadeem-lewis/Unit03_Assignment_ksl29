8410db329c99b430e292c5ff9017dcc3076d0ea8
kadeem-lewis <email@kadeemlewis.com>
2023-11-29 02:22:57 -0500

    styled homepage cards to look more like cards from pokemon.com

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -1,3 +1,9 @@
+//Kadeem Lewis
+//ksl29
+//12-01-2023
+//IT301 - 001
+//Unit 11 Assignment
+
 import React from "react";
 import styles from "./PokemonCard.module.css";
 import { Link } from "react-router-dom";
@@ -10,7 +16,18 @@ export default function PokemonCard({ pokemon }) {
         alt={`${pokemon.name} sprite`}
         className={styles.image}
       />
-      <p>{pokemon.name}</p>
+
+      <p className={styles.pokeId}>#{String(pokemon.id).padStart(3, "0")}</p>
+      <div>{pokemon.name}</div>
+      <span>
+        {pokemon.types
+          .filter((type) => type !== "none")
+          .map((type) => (
+            <span key={type} className={`type ${type}`}>
+              {type}
+            </span>
+          ))}
+      </span>
     </Link>
   );
 }
