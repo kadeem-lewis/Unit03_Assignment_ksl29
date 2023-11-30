d6e079249213e0108903efdcd28380d31952d553
kadeem-lewis <email@kadeemlewis.com>
2023-11-30 06:15:44 -0500

    updated add comment loader to handle post and put requests

--- frontend/src/routes/addComment.jsx
+++ frontend/src/routes/addComment.jsx
@@ -27,7 +27,7 @@ export default function AddComment() {
 
   return (
     <>
-      <Form method="post">
+      <Form method={editing ? "put" : "post"}>
         <textarea
           name="comment"
           id="comment"
@@ -38,6 +38,11 @@ export default function AddComment() {
         />
         <input type="hidden" name="username" value={user.username} />
         <input type="hidden" name="userId" value={user.userId} />
+        <input
+          type="hidden"
+          name="commentId"
+          value={state?.currentComment._id}
+        />
         <button type="submit">Submit</button>
       </Form>
     </>
@@ -49,13 +54,39 @@ export async function commentAction({ request, params }) {
   const comment = formData.get("comment");
   const username = formData.get("username");
   const userId = formData.get("userId");
-  const response = await fetch("http://localhost:5000/api/v1/ksl29/comments", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ pokemonId: params.id, comment, username, userId }),
-  });
-  const data = await response.json();
-  return data;
+  const commentId = formData.get("commentId");
+  switch (request.method) {
+    case "POST":
+      const response = await fetch(
+        "http://localhost:5000/api/v1/ksl29/comments",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            pokemonId: params.id,
+            comment,
+            username,
+            userId,
+          }),
+        },
+      );
+      const data = await response.json();
+      return data;
+    case "PUT":
+      const res = await fetch("http://localhost:5000/api/v1/ksl29/comments", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          comment,
+          commentId,
+          userId,
+        }),
+      });
+      const results = await res.json();
+      return results;
+  }
 }
